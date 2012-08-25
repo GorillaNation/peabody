d1f97cc28ab89cc9789fb0dbbee72e0660209a9b
Jeremy Kitchen <jeremy.kitchen@gorillanation.com>
2012-08-25 13:11:52 -0700

    final pipification, I think!

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Peabody',
+    version='0.2.0',
+    author='Jeremy Kitchen',
+    author_email='jeremy.kitchen@gorillanation.com',
+    packages=['peabody'],
+    scripts=['bin/peabody'],
+    url='https://bitbucket.org/kitchen/peabody',
+    license='LICENSE.txt',
+    description='wrapper for cronjobs to provide timeouts, locking, and some other fun features',
+    install_requires=[
+        "lockfile",
+    ],
+)
