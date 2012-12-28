2de57bc1c3ae1ff022178306370f8e925b6a056f
Jeremy Kitchen <jeremy.kitchen@gorillanation.com>
2012-12-28 13:12:08 -0800

    fully configurable logstash redis output

--- peabody/output/logstash_redis.py
+++ peabody/output/logstash_redis.py
@@ -1,37 +1,47 @@
 import redis
 import json
-from peabody.output import Output
+import peabody.output
 import redis
 import socket
 from datetime import datetime
 import uuid
+from urlparse import urlparse
+import re
 
-class LogstashRedis(Output):
+class LogstashRedis(peabody.output.Output):
     def __init__(self, options):
-        #super(Output, self).__init__(options)
-        self.redis = redis.StrictRedis(host="jupiter.home.kitchen.io", port=6379, db=0)
+        super(LogstashRedis, self).__init__(options)
+        redis_url = urlparse(options.logstash_redis_url)
+        self.redis = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, db=re.sub(r'^/', '', redis_url.path))
 
         self.obj = {
             '@fields': {
-                    'peabody_job_id': uuid.uuid4().hex,
+                'peabody': {
+                    "run_id": options.cronjob_run_id,
+                    "child_pid": options.child_pid,
+                }
             },
             '@type':'peabody',
-            '@source_host':socket.getfqdn(),
-            '@source_path':'peabody_script_thing',
+            '@source_host': options.logstash_source_host,
+            '@source_path': options.logstash_source_file,
+            '@source': "file://{0}/{1}".format(options.logstash_source_host, options.logstash_source_file),
         }
 
-        self.obj['@source'] = "file://{0}/{1}".format(self.obj["@source_host"], self.obj["@source_path"])
+        if options.cronjob_name:
+            self.obj["@fields"]["peabody"]["cronjob"] = options.cronjob_name
 
-        self.redis_key = 'logstash'
+        self.redis_key = options.logstash_redis_key
         
     def stdout(self, line):
-        self.obj["@timestamp"] = datetime.utcnow().isoformat('T') + 'Z'
-        self.obj["@message"] = line
-        self.obj["@fields"]["peabody_output"] = "stdout"
-        self.redis.rpush(self.redis_key, json.dumps(self.obj))
+        self.obj["@fields"]["peabody"]["output"] = "stdout"
+        self.output(line)
 
     def stderr(self, line):
+        self.obj["@fields"]["peabody"]["output"] = "stderr"
+        self.output(line)
+
+    def output(self, line):
         self.obj["@timestamp"] = datetime.utcnow().isoformat('T') + 'Z'
         self.obj["@message"] = line
-        self.obj["@fields"]["peabody_output"] = "stderr"
         self.redis.rpush(self.redis_key, json.dumps(self.obj))
+
