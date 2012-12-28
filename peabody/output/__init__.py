7cd9603b1f1d08ec520a7553f5b8651273111f7d
Jeremy Kitchen <jeremy.kitchen@gorillanation.com>
2012-12-27 20:34:49 -0800

    logstash redis output!
    it works. now I just need to make it ... less crappy :)

---
+++ peabody/output/__init__.py
@@ -0,0 +1,11 @@
+class Output(object):
+    def __init__(self, options):
+        pass
+
+    # stdout callback
+    def stdout(self, line):
+        pass
+
+    # stderr callback
+    def stderr(self, line):
+        pass
---
+++ peabody/output/logstash_redis.py
@@ -0,0 +1,40 @@
+import redis
+import json
+from peabody.output import Output
+import redis
+import socket
+from datetime import datetime
+import uuid
+
+class LogstashRedis(Output):
+    def __init__(self, options):
+        #super(Output, self).__init__(options)
+        self.redis = redis.StrictRedis(host="jupiter.home.kitchen.io", port=6379, db=0)
+
+        self.obj = {
+            '@fields': {
+                    'peabody:job_id': uuid.uuid4().hex,
+            },
+            '@type':'peabody',
+            '@source_host':socket.getfqdn(),
+            '@source_path':'peabody_script_thing',
+        }
+
+        self.obj['@source'] = "file://{0}/{1}".format(self.obj["@source_host"], self.obj["@source_path"])
+
+        self.redis_key = 'logstash'
+        
+    def stdout(self, line):
+        self.obj["@timestamp"] = datetime.utcnow().isoformat('T') + 'Z'
+        self.obj["@message"] = line
+        self.obj["@fields"]["peabody:output"] = "stdout"
+        self.redis.rpush(self.redis_key, json.dumps(self.obj))
+
+    def stderr(self, line):
+        self.obj["@timestamp"] = datetime.utcnow().isoformat('T') + 'Z'
+        self.obj["@message"] = line
+        self.obj["@fields"]["peabody:output"] = "stderr"
+        self.redis.rpush(self.redis_key, json.dumps(self.obj))
+
+
+
